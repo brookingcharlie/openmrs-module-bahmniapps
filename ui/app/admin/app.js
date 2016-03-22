2ce2c42609dbaf8944b12021d2e1095c61191477
chethana <chethana@INchethana-2.local>
2016-03-22 18:27:42 +0530

    Chetan, Gautam | #1045 | Deleting drug object if drug name is empty while authoring an orderset.
    Refactored AddTreatmentController.

--- ui/app/admin/app.js
+++ ui/app/admin/app.js
@@ -1,67 +1,61 @@
 'use strict';
 
-angular.module('admin', ['httpErrorInterceptor', 'bahmni.admin', 'bahmni.common.routeErrorHandler', 'ngSanitize', 'bahmni.common.uiHelper',
-    'bahmni.common.config', 'bahmni.common.orders', 'bahmni.common.i18n', 'pascalprecht.translate', 'ngCookies', 'angularFileUpload', 'bahmni.common.offline'])
-    .config(['$stateProvider', '$httpProvider', '$urlRouterProvider', '$bahmniTranslateProvider', function ($stateProvider, $httpProvider, $urlRouterProvider, $bahmniTranslateProvider) {
-        $urlRouterProvider.otherwise('/dashboard');
-    $stateProvider.state('admin', {
-        abstract: true,
-        template: '<ui-view/>',
-        resolve: {
-            initialize: 'initialization'
-        }
-    }).state('admin.dashboard',
-        {   url: '/dashboard',
-            templateUrl: 'views/adminDashboard.html',
-            controller: 'AdminDashboardController',
-            data: {
-                backLinks: [{label: "Home", accessKey: "h", url: "../home/", icon: "fa-home"}],
-                extensionPointId: 'org.bahmni.admin.dashboard'
-            }
-        })
-        .state('admin.csv',
-        {   url: '/csv',
-            templateUrl: 'views/csvupload.html',
-            controller: 'CSVUploadController'
+angular.module('admin', ['httpErrorInterceptor', 'bahmni.admin', 'bahmni.common.routeErrorHandler', 'ngSanitize',
+    'bahmni.common.uiHelper', 'bahmni.common.config', 'bahmni.common.orders', 'bahmni.common.i18n', 'pascalprecht.translate',
+    'ngCookies', 'angularFileUpload', 'bahmni.common.offline', 'bahmni.common.services']);
+angular.module('admin')
+    .config(['$stateProvider', '$httpProvider', '$urlRouterProvider',
+        function ($stateProvider, $httpProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/dashboard');
+            $stateProvider.state('admin', {
+                abstract: true,
+                template: '<ui-view/>',
+                resolve: {
+                    initialize: 'initialization'
+                }
+            }).state('admin.dashboard', {
+                url: '/dashboard',
+                templateUrl: 'views/adminDashboard.html',
+                controller: 'AdminDashboardController',
+                data: {
+                    backLinks: [{label: "Home", accessKey: "h", url: "../home/", icon: "fa-home"}],
+                    extensionPointId: 'org.bahmni.admin.dashboard'
+                }
+            }).state('admin.csv', {
+                url: '/csv',
+                templateUrl: 'views/csvupload.html',
+                controller: 'CSVUploadController'
 
-        })
-        .state('admin.csvExport', {   
-            url: '/csvExport',
-            templateUrl: 'views/csvexport.html',
-            controller: 'CSVExportController'
+            }).state('admin.csvExport', {
+                url: '/csvExport',
+                templateUrl: 'views/csvexport.html',
+                controller: 'CSVExportController'
 
-        })
-        .state('admin.formBuilder',
-        {   url: '/formBuilder',
-            templateUrl: 'views/formBuilder.html',
-            controller: ''
-        })
-        .state('admin.formIndex',
-        {   url: '/formIndex',
-            templateUrl: 'views/formIndex.html',
-            controller: ''
-        })
-            .state('admin.orderSetDashboard', {
+            }).state('admin.formBuilder', {
+                url: '/formBuilder',
+                templateUrl: 'views/formBuilder.html',
+                controller: ''
+            }).state('admin.formIndex', {
+                url: '/formIndex',
+                templateUrl: 'views/formIndex.html',
+                controller: ''
+            }).state('admin.orderSetDashboard', {
                 url: '/ordersetdashboard',
                 templateUrl: 'views/orderSetDashboard.html',
                 controller: 'OrderSetDashboardController',
                 data: {
-                    backLinks: [{label: "Home", state: "admin.dashboard", icon: "fa-home"}],
+                    backLinks: [{label: "Home", state: "admin.dashboard", icon: "fa-home"}]
                 }
-            })
-            .state('admin.orderSet', {
+            }).state('admin.orderSet', {
                 url: '/orderset/:orderSetUuid',
                 templateUrl: 'views/orderSet.html',
                 data: {
-                    backLinks: [{label: "Home", state: "admin.orderSetDashboard", icon: "fa-users"}],
+                    backLinks: [{label: "Home", state: "admin.orderSetDashboard", icon: "fa-users"}]
                 }
             });
-        $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
-    }]).
-    run(function ($rootScope, $templateCache) {
+            $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
+        }
+    ]).run(function ($rootScope, $templateCache) {
         //Disable caching view template partials
-        $rootScope.$on('$viewContentLoaded', function () {
-                $templateCache.removeAll();
-            }
-        )
+        $rootScope.$on('$viewContentLoaded', $templateCache.removeAll);
     });
